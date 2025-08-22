d137a6d5cbf46c93ce6563fd5f110e0eb386f39d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-08-22 12:09:15 +0200

    feat: show hidden files in oil

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
