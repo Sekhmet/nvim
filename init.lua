17dd985826a679580f1ff3f2a6fadaa26b61112d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-08-22 18:43:21 +0200

    feat: show command line when recording macro

--- init.lua
+++ init.lua
@@ -2,10 +2,21 @@ require("config.lazy")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.cmdheight = 0
-
 vim.opt.cursorline = true
-
 vim.opt.scrolloff = 8
 
 vim.diagnostic.config({ virtual_text = true })
+
+-- Command height
+vim.opt.cmdheight = 0
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+	callback = function()
+		vim.opt.cmdheight = 1
+	end,
+})
+vim.api.nvim_create_autocmd("RecordingLeave", {
+	callback = function()
+		vim.opt.cmdheight = 0
+	end,
+})
